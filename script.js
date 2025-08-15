c29e0e5372a986b5682ee3b112217a07ece89699
Burati03 <gabrielhenrique@colegiopaulodetarso.com.br>
2025-08-15 22:51:37 +0000

    portifolio

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirPreview(url) {
+  window.open(url, "_blank");
+}
+
+// Exemplo: botão para trocar tema no futuro
+function trocarTema() {
+  document.body.classList.toggle("tema-claro");
+}
